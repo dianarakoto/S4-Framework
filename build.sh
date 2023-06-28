431279ca8ee05fb9f89aeda4f33cd93e6e844b34
Diana Rakotomaharo <dianarakoto9@gmail.com>
2023-06-28 13:04:10 +0300

    Inputs

--- build.sh
+++ build.sh
@@ -4,21 +4,35 @@ TEST_FRAMEWORK="//home/artemis/Documents/Creation/Test-Framework"
 CLASSES="$TEST_FRAMEWORK/WEB-INF/classes"
 TEST_LIB="$TEST_FRAMEWORK/WEB-INF/lib"
 TOMCAT="/home/artemis/apache-tomcat-10.1.2/webapps"
+TEMP="/home/artemis/Documents/Creation/Temp"
+MODEL="/home/artemis/Documents/Creation/Temp/WEB-INF/classes"
+LIB="/home/artemis/Documents/Creation/Temp/WEB-INF/lib"
 
-cd ./Source-Framework/src/java
 FILES=$(find . -name "*.java")
 for FILE in $FILES
 do
     cp $FILE $DEST
 done
+
 cd $DEST
 javac -d . *.java
 rm *.java
 jar -cf framework.jar etu2000
-cp framework.jar $TEST_LIB
-cd $CLASSES
-javac -cp $TEST_LIB/framework.jar -d . *.java
+cp framework.jar $LIB
+
 cd $TEST_FRAMEWORK
+JAVA=$(find . -name "*.java")
+for CLASS in $JAVA
+do
+    cp $JAVA $MODEL
+done
+
+cd $MODEL
+javac -cp $LIB/framework.jar -d . *.java
+
+cp $TEST_FRAMEWORK/*.jsp $TEMP
+cp $TEST_FRAMEWORK/WEB-INF/web.xml $TEMP/WEB-INF
+cd $TEMP
 jar cvf Employees.war .
 cp Employees.war $TOMCAT
 rm Employees.war
---
+++ create_temp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir //home/artemis/Documents/Creation/Temp
+mkdir //home/artemis/Documents/Creation/Temp/WEB-INF/
+mkdir //home/artemis/Documents/Creation/Temp/WEB-INF/classes/
+mkdir //home/artemis/Documents/Creation/Temp/WEB-INF/lib/

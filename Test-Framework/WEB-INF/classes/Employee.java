6ab482ea1e4a6f1265d32bbf3e13e13d3c0f5f9c
Diana Rakotomaharo <dianarakoto9@gmail.com>
2023-06-28 12:57:20 +0300

    Send data

--- Test-Framework/WEB-INF/classes/Employee.java
+++ Test-Framework/WEB-INF/classes/Employee.java
@@ -1,6 +1,8 @@
 package model;
 
 import etu2000.framework.annotation.Url;
+import etu2000.framework.ModelView;
+import java.util.Vector;
 
 public class Employee {
     int id;
@@ -26,7 +28,13 @@ public class Employee {
     }
     
     @Url("find-emp")
-    public void findAll(){
-        System.out.println("All employees");
+    public ModelView findAll(){
+        ModelView view = new ModelView("test.jsp");
+        Vector<String> noms = new Vector<>();
+        noms.add("Diana");
+        noms.add("Megane");
+        // view.setView("list.jsp");
+        view.addItem("allEmployees", noms);
+        return view;
     }
 }
